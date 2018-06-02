c7773092508c502739f63df233a1efaab16a0c94
Tulrose Deori <tulrosdeori@gmail.com>
2018-06-02 21:35:08 +0530

    bug fix

--- app/src/config.js
+++ app/src/config.js
@@ -1,5 +1,6 @@
-export const production = true; // set it to true when deploy to the server
+// export const production = true; // set it to true when deploy to the server
 
-const domain = production ? 'pacific-citadel-79750.herokuapp.com' : '127.0.0.1:3001'; // if you have domain pointed to digitalOcean Cloud server let use your domain.eg: tabvn.com
+// const domain = production ? 'pacific-citadel-79750.herokuapp.com' : '127.0.0.1:3001'; // if you have domain pointed to digitalOcean Cloud server let use your domain.eg: tabvn.com
+const domain = '127.0.0.1:3001'; // if you have domain pointed to digitalOcean Cloud server let use your domain.eg: tabvn.com
 export const websocketUrl = `ws://${domain}`
 export const apiUrl = `http://${domain}`
