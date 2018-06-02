7aa36a5f68328eb7c8108fc1c2e37d332e204698
Tulrose Deori <tulrosdeori@gmail.com>
2018-06-02 21:06:42 +0530

    bug fix

--- src/index.js
+++ src/index.js
@@ -32,14 +32,14 @@ app.wss = new Server({
 });
 
 
-// static www files use express
-const wwwPath = path.join(__dirname, 'www');
+// // static www files use express
+// const wwwPath = path.join(__dirname, 'www');
 
-app.use('/', express.static(wwwPath));
+// app.use('/', express.static(wwwPath));
 
-if (process.env.NODE_ENV === "production") {
-	app.use(express.static("client/build"));
-}
+// if (process.env.NODE_ENV === "production") {
+	app.use('/', express.static(__dirname, '../..', 'client/build'));
+// }
 
 // Connect to Mongo Database
 
